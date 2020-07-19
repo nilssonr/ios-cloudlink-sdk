4976183685ecfce07b1e17cb502457cefb93b53e
Robin Nilsson <robin.nilsson@mitel.com>
2020-07-19 02:16:41 +0200

    Fixed one..

--- Sources/CloudlinkSDK/Authentication/Models/CLTokenRequest.swift
+++ Sources/CloudlinkSDK/Authentication/Models/CLTokenRequest.swift
@@ -1,6 +1,13 @@
 public struct CLTokenRequest {
-    public var accountId: String;
-    public var username: String;
-    public var password: String;
-    public var grantType: String;
+    var accountId: String;
+    var username: String;
+    var password: String;
+    var grantType: String;
+    
+    public init(accountId: String, username: String, password: String, grantType: String) {
+        self.accountId = accountId
+        self.username = username
+        self.password = password
+        self.grantType = grantType
+    }
 }
